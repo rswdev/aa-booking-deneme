4de9a7942eba1cbca05f9b19fa58000546997753
Eren Mercan <59459366+rswdev@users.noreply.github.com>
2020-06-21 21:37:22 +0300

    Update create-records.js

--- create-records.js
+++ create-records.js
@@ -7,28 +7,28 @@ const { passengerDatabase, driverDatabase } = require('./database')
 const printBookingHistory = require('./lib/print-booking-history')
 
 
-// const eren = Passenger.create({name: 'Eren MERCAN', location: 'Bayrakli'})
-// const ozlem = Passenger.create({name : 'Ozlem GERGUS', location:'Camdibi'})
+const eren = Passenger.create({name: 'Eren MERCAN', location: 'Bayrakli'})
+const ozlem = Passenger.create({name : 'Ozlem GERGUS', location:'Camdibi'})
 const ahmet = Driver.create({name : 'Ahmet Tyrell', location: 'Alsancak'})
 
 
-// eren.book(ahmet,'Bayrakli', 'Forum Bornova')
-// eren.book(ahmet,'Forum Bornova', 'Karsiyaka')
-// eren.book(ahmet,'Karsıyaka', 'Bostanlı')
-// ozlem.book(ahmet,'Bayrakli', 'Forum Bornova')
-// // burhan.book(ahmet, 'Tokat', 'Tokat/Erbag')
+eren.book(ahmet,'Bayrakli', 'Forum Bornova')
+eren.book(ahmet,'Forum Bornova', 'Karsiyaka')
+eren.book(ahmet,'Karsıyaka', 'Bostanlı')
+ozlem.book(ahmet,'Bayrakli', 'Forum Bornova')
+// burhan.book(ahmet, 'Tokat', 'Tokat/Erbag')
 
 
 
-// passengerDatabase.save([eren, ozlem])  // passenger.json'ı oluştur ve kaydet.
-// // driverDatabase.save([ahmet])           // drivers.json'u oluştur ve kaydet. 
+passengerDatabase.save([eren, ozlem])  // passenger.json'ı oluştur ve kaydet.
+driverDatabase.save([ahmet])           // drivers.json'u oluştur ve kaydet. 
 
 
 // // const burhan = Passenger.create({name : 'Burhan Altıntop', location:'Tokat/Erbag'})
 
 // // passengerDatabase.insert(burhan)
 
-// const passengers = passengerDatabase.load()
+const passengers = passengerDatabase.load()
 
-// passengers.forEach(printBookingHistory)
+passengers.forEach(printBookingHistory)
 
